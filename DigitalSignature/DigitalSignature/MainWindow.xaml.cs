4bc5282404312c370112231eed4cbe186b7aca31
Andrey <84859114+Andrey1039@users.noreply.github.com>
2023-03-17 22:56:38 +0500

    Добавлена программа

---
+++ DigitalSignature/DigitalSignature/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Numerics;
+using DigitalSignature.Data;
+using DigitalSignature.Signature;
+
+namespace DigitalSignature
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Signature()
+        {
+            BigInteger p = BigInteger.Parse(
+                "57896044618658097711785492504343953926634992332820282019728792003956564821041");
+            BigInteger a = BigInteger.Parse("7");
+            BigInteger b = BigInteger.Parse(
+                "43308876546767276905765904595650931995942111794451039583252968842033849580414");
+            BigInteger q = BigInteger.Parse(
+                "57896044618658097711785492504343953927082934583725450622380973592137631069619");
+            BigInteger xP = BigInteger.Parse("2");
+            BigInteger yP = BigInteger.Parse(
+                "4018974056539037503335449422937059775635739389905545080690979365213431566280");
+
+            // Создание ЭЦП
+            EllipticPoint point = new EllipticPoint(a, b, p, xP, yP);
+            DigSignature genSignature = new DigSignature(point, q);
+
+            BigInteger d = genSignature.GeneratePrivateKey(32);
+            EllipticPoint pointQ = genSignature.GeneratePublicKey(d);
+
+            SignatureTB.Text = Convert.ToHexString(genSignature.GetSignature(InputTextTB.Text, d));
+
+            point = new EllipticPoint(a, b, p, xP, yP);
+            DigSignature checkSignature = new DigSignature(point, q);
+
+            if (checkSignature.CheckSignature(InputText2TB.Text, Convert.FromHexString(SignatureTB.Text), pointQ))
+                MessageBox.Show("Проверка пройдена!");
+            else
+                MessageBox.Show("Проверка провалена!");
+        }
+
+        private void ExecuteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Signature();
+        }
+
+        private void VerificationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Signature();
+        }
+
+        private void InputTextTB_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!InputTextTB.Text.Equals(string.Empty) && !InputText2TB.Text.Equals(string.Empty))
+                ExecuteBtn.IsEnabled = true;
+            else
+                ExecuteBtn.IsEnabled = false;
+        }
+    }
+}
---
+++ DigitalSignature/DigitalSignature/Signature/DigSignature.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Numerics;
+using DigitalSignature.Hash;
+using DigitalSignature.Data;
+using System.Security.Cryptography;
+
+namespace DigitalSignature.Signature
+{
+    internal class DigSignature
+    {
+        BigInteger q;
+        int hash_length;
+        EllipticPoint point;
+
+        public DigSignature(EllipticPoint point, BigInteger q)
+        {
+            this.point = point;
+            this.q = q;
+
+            if (q > BigInteger.Pow(2, 254) && q < BigInteger.Pow(2, 256))
+                hash_length = 256;
+            else
+                if (q > BigInteger.Pow(2, 508) && q < BigInteger.Pow(2, 512))
+                hash_length = 512;
+            else
+                throw new Exception("Неверное значение q");
+        }
+        
+        // Генерация случайных чисел
+        private BigInteger GetRandomNumber(int length)
+        {
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                var randomBytes = new byte[length];
+                generator.GetBytes(randomBytes);
+                BigInteger result = new BigInteger(randomBytes);
+
+                return BigInteger.Abs(result);
+            }
+        }
+
+        // Генерация закрытого ключа
+        public BigInteger GeneratePrivateKey(int length)
+        {
+            BigInteger d = new BigInteger();
+
+            do
+            {
+                d = GetRandomNumber(length);
+            }
+            while (d < 1 || d > q);
+
+            return d;
+        }
+
+        // Генерация открытого ключа
+        public EllipticPoint GeneratePublicKey(BigInteger d)
+        {
+            return point * d;
+        }
+
+        // Вычисление хэша
+        private byte[] Hash(string message)
+        {
+            Streebog streebog = new Streebog();
+            byte[] hash = Array.Empty<byte>();
+
+            if (hash_length == 256)
+                hash = streebog.GetHash256(message);
+            else
+                hash = streebog.GetHash512(message);
+
+            return hash;
+        }
+
+        // Вычисление значения k
+        private BigInteger GetK()
+        {
+            BigInteger k;
+
+            do
+            {
+                k = GetRandomNumber(hash_length / 8);
+            }
+            while (k < 1 || k > q);
+
+            return k;
+        }
+
+        // Формирование ЭЦП
+        public byte[] GetSignature(string message, BigInteger d)
+        {
+            byte[] hash = Hash(message);
+
+            BigInteger alpha = new BigInteger(hash);
+            BigInteger e = Mathematics.Mod(alpha, q) == 0 ? 1 : Mathematics.Mod(alpha, q);
+
+            BigInteger k;
+            BigInteger r;
+            BigInteger s;
+
+            do
+            {
+                do
+                {
+                    k = GetK();
+                    
+                    EllipticPoint C = point * k;
+                    r = Mathematics.Mod(C.X, q);
+                }
+                while (r == 0);
+
+                s = Mathematics.Mod(r * d + k * e, q);
+            }
+            while (s == 0);
+
+            byte[] signature = new byte[hash_length / 4];
+
+            Array.Copy(r.ToByteArray(), 0, signature, 0, hash_length / 8);
+            Array.Copy(s.ToByteArray(), 0, signature, hash_length / 8, hash_length / 8);
+
+            return signature;
+        }
+
+        // Проверка ЭЦП
+        public bool CheckSignature(string message, byte[] signature, EllipticPoint Q)
+        {
+            byte[] byte_r = new byte[hash_length / 8];
+            byte[] byte_s = new byte[hash_length / 8];
+
+            Array.Copy(signature, 0, byte_r, 0, hash_length / 8);
+            Array.Copy(signature, hash_length / 8, byte_s, 0, hash_length / 8);
+
+            BigInteger r = new BigInteger(byte_r);
+            BigInteger s = new BigInteger(byte_s);
+
+            if (r < 1 || r > q - 1 || s < 1 || s > q - 1)
+                return false;
+
+            byte[] hash = Hash(message);
+
+            BigInteger alpha = new BigInteger(hash);
+            BigInteger e = Mathematics.Mod(alpha, q) == 0 ? 1 : Mathematics.Mod(alpha, q);
+
+            BigInteger v = Mathematics.Ext_Euclidian(e, q);
+
+            BigInteger z1 = Mathematics.Mod(s * v, q);
+            BigInteger z2 = Mathematics.Mod(-(r * v), q);
+
+            EllipticPoint C = z1 * point + z2 * Q;
+
+            BigInteger R = Mathematics.Mod(C.X, q);
+
+            if (R == r)
+                return true;
+
+            return false;
+        }
+    }
+}
